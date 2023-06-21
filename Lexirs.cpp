d7484e6cc1d67ee54096eed145511b31cb0e44d1
mzib18 <118097810+mzib18@users.noreply.github.com>
2023-06-21 12:51:53 +0530

    Add files via upload

---
+++ Lexirs.cpp
@@ -0,0 +1,19 @@
+    #include<bits/stdc++.h>
+    using namespace std;
+
+    #define ll long long int
+    #define LD long double
+    #define IOS ios_base::sync_with_stdio(0);cin.tie(0); cout.tie(0);
+    const int N = 100010;
+
+    int inf = 1e9;
+    int mod = 1e9 + 7;
+    signed main(){
+        IOS
+        int T;
+        cin>>T;
+        while(T--){
+
+        }
+
+    }
