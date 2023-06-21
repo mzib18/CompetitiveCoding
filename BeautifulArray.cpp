e6d318baa32477354de17b1a73df31ca2276feeb
mzib18 <118097810+mzib18@users.noreply.github.com>
2023-06-21 12:29:19 +0530

    Add files via upload

---
+++ BeautifulArray.cpp
@@ -0,0 +1,20 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+#define ll long long int
+#define LD long double
+#define IOS ios_base::sync_with_stdio(0);cin.tie(0); cout.tie(0);
+const int N = 100010;
+
+int inf = 1e9;
+int mod = 1e9 + 7;
+signed main(){
+IOS
+int T;cin>>T;
+while(T--){
+    int n;cin>>n;
+    vector<int> arr(n,0);
+    for(int i=0;i<n;i++)cin>>arr[i];
+
+}
+}
